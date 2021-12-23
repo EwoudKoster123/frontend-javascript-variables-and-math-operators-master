30f9d68e589237788ab876bd964cb56f5769e593
EwoudKoster123 <ewoudbasketbal@gmail.com>
2021-12-23 12:55:11 +0100

    Initial commit

---
+++ variables.js
@@ -0,0 +1,46 @@
+// ==========================================
+// 0. Voorbeeldvraag: maak en variabele genaamd start met daarin het woord Let's go!
+// ==========================================
+
+const start = "Let's go!";
+
+// ==========================================
+// 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
+// ==========================================
+var firstName = "Ewoud";
+
+// ==========================================
+// 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
+// ==========================================
+var eyeColor = "Green";
+
+// ==========================================
+// 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
+// ==========================================
+var age = 19;
+
+// ==========================================
+// 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
+// ==========================================
+var beroep = "hbo ict student";
+
+// ==========================================
+// 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
+// ==========================================
+var shoeSize = 48.5;
+
+// ==========================================
+// 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
+// ==========================================
+var partnerYesNo = "geen";
+
+// ==========================================
+// 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
+// ==========================================
+var children = "none";
+
+// ==========================================
+// 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
+// ==========================================
+console.log(start + " " + firstName + " met de oogkleur " + eyeColor + " die " + age + " jaar oud is en schoenmaat " + shoeSize + " heeft en daarbij " + partnerYesNo + " en " + children + " heeft.");
+
